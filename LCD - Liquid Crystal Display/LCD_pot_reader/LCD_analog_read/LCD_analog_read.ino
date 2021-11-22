695280e42dd1c839ab96ae8fa74303bc7887d05d
dawson <dawsonreschke@icloud.com>
2021-11-21 16:51:27 -0800

    LCD Displaying USEFULL DATA???

--- LCD - Liquid Crystal Display/LCD_pot_reader/LCD_analog_read/LCD_analog_read.ino
+++ LCD - Liquid Crystal Display/LCD_pot_reader/LCD_analog_read/LCD_analog_read.ino
@@ -1,31 +1,37 @@
+/*
+ * -> Dawson Reschke <-
+ * 
+ * This sketch displays analogRead value onto a Liquid Crystal Display
+ * 
+ * uses : external libs, analog, & I2C.
+ */
+
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
 LiquidCrystal_I2C lcd(0x27,20,4);
 const int POT_PIN = A0; 
-const float MAP_READ = 0.00488758553; 
+const float MAP_READ = 0.00488758553; // value used to map the 0-1023 to 0-5 
 
 
 void setup() {
   pinMode(POT_PIN,INPUT_PULLUP); 
   lcd.init(); 
   updateLCD(); 
-  Serial.begin(9600); 
 }
 
 void updateLCD(){
   int potValue = analogRead(POT_PIN); // returns value from 0-1023
-  Serial.println(potValue); 
   lcd.clear(); 
   lcd.backlight();   
   lcd.setCursor(0,0); 
   lcd.print("analog read: ");
   lcd.setCursor(0,1); 
   lcd.print(((float)potValue * MAP_READ)); 
-  lcd.print("V"); 
+  lcd.print("V"); // this data is not exactly accurate
 }
 
 void loop() {
-  delay(100); 
+  delay(100); // delay because we clear display in updateLCD. If we left delay out the lcd would always be blank. 
   updateLCD(); 
 }
