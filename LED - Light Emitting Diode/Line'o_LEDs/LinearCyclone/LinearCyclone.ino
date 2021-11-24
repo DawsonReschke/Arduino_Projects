f9f0a3a3e76c6025bbf2976baabb78a4ed6296f1
Dawson <dawsonreschke@icloud.com>
2021-11-24 12:47:29 -0800

    Added interrupt & ending functionality. Still need to test on hardware...

--- LED - Light Emitting Diode/Line'o_LEDs/LinearCyclone/LinearCyclone.ino
+++ LED - Light Emitting Diode/Line'o_LEDs/LinearCyclone/LinearCyclone.ino
@@ -5,15 +5,20 @@
  */
 
 const int OUTPUTPINS[] = {13,12,11,10,9,8,7,6,5,4,3};
+const int len = sizeof(OUTPUTPINS) / sizeof(OUTPUTPINS[0]); // in C++ there is not .length for arrays. so we must use this sizeof tech
 const int BUTTONPIN = 2;
 const int POTPIN = A0;
 
-int NEXT_LED_FRAMECOUNT = -1; 
-int CURRENT_LED_INDEX = 0; 
-int FRAMECOUNT = 0; 
+volatile boolean BUTTON_PRESSED = false; 
+
+int NEXT_LED_FRAMECOUNT; 
+int CURRENT_LED_INDEX;
+int FRAMECOUNT;
 
 void setup() {
-  int len = sizeof(OUTPUTPINS) / sizeof(OUTPUTPINS[0]); 
+  NEXT_LED_FRAMECOUNT = -1; 
+  CURRENT_LED_INDEX = 0; 
+  FRAMECOUNT = 0; 
   for(int i = 0; i < len; i++){
     pinMode(OUTPUTPINS[i],OUTPUT);
   }
@@ -22,6 +27,9 @@ void setup() {
 }
 
 void loop() {
+  if(BUTTON_PRESSED){
+    evaluate(); 
+  }
   updateNextFramecount(); 
   if(FRAMECOUNT < NEXT_LED_FRAMECOUNT){
     FRAMECOUNT++; 
@@ -37,8 +45,36 @@ void updateNextFramecount(){
 }
 
 void nextLED(){
-  int len = sizeof(OUTPUTPINS)/sizeof(OUTPUTPINS[0]); 
   digitalWrite(OUTPUTPINS[CURRENT_LED_INDEX],LOW); 
   CURRENT_LED_INDEX = CURRENT_LED_INDEX < len ? CURRENT_LED_INDEX + 1 : 0;
   digitalWrite(OUTPUTPINS[CURRENT_LED_INDEX],HIGH); 
+}
+
+void evaluate(){
+  bool hasWon = CURRENT_LED_INDEX == (len/2);
+  // if the game is won blink LEDS individuly
+  if(hasWon){
+    for(int i = 0; i < 1; i++){
+      for(int j = 0; j < len; j++){
+        digitalWrite(OUTPUTPINS[j],HIGH); 
+        delay(250); 
+        digitalWrite(OUTPUTPINS[j],LOW); 
+      }
+    }
+  }else{
+     // else blink the leds once then restart
+     for(int i = 0; i < len ; i++){
+        digitalWrite(OUTPUTPINS[i],HIGH); 
+     }
+     delay(500); 
+     for(int i = 0; i < len; i++){
+        digitalWrite(OUTPUTPINS[i],LOW); 
+     }
+  }
+ 
+}
+
+
+void interruptHandler(){ // after the button is pressed the program should recognize a win or a loss.
+  BUTTON_PRESSED = true; 
 }
